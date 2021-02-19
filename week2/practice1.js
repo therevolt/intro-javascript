216d0041830e82008ce30d9a4e97f3c69ca23347
Rama Seftiansyah <ramaseftiansyah12@gmail.com>
2021-02-19 12:52:38 +0700

    Add Practice1.js in week2 Folder

---
+++ week2/practice1.js
@@ -0,0 +1,24 @@
+// Latihan Callback Untuk Menampung Hasil Ajax
+
+const mapData = (data) => {
+  data.map((item) => {
+    console.log(item.title);
+  });
+};
+
+const getDataAjax = (url, method, callback) => {
+  const ajax = new XMLHttpRequest();
+  ajax.open(method, url);
+  ajax.send();
+
+  ajax.onreadystatechange = function () {
+    if (ajax.status === 200) {
+      const result = JSON.parse(ajax.responseText);
+      callback(result);
+    } else {
+      console.log(`Server Error!`);
+    }
+  };
+};
+
+getDataAjax("https://jsonplaceholder.typicode.com/posts", "GET", mapData);
